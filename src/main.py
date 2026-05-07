b8b82291023161e133221b3b2ba6289885634efc
jcpachon18-alt <jcpachon18@gmail.com>
2026-05-06 22:55:52 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,35 @@
+from excepciones import (
+    ClienteInvalidoError,
+    ServicioNoDisponibleError,
+    ReservaError
+)
 
+
+print("=== INICIO DEL SISTEMA ===")
+
+
+# PRUEBA 1: Error de cliente
+try:
+    raise ClienteInvalidoError()
+
+except ClienteInvalidoError as error:
+    print(f"Error detectado: {error}")
+
+
+# PRUEBA 2: Error de servicio
+try:
+    raise ServicioNoDisponibleError()
+
+except ServicioNoDisponibleError as error:
+    print(f"Error detectado: {error}")
+
+
+# PRUEBA 3: Error de reserva
+try:
+    raise ReservaError()
+
+except ReservaError as error:
+    print(f"Error detectado: {error}")
+
+
+print("=== EL SISTEMA SIGUE FUNCIONANDO ===")
